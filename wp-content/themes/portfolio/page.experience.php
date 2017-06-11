7fe6be0046bc7fa790290cb5d69c22dfa30b5194
Treisy Jimenez <treisyjimenez@gmail.com>
2017-06-11 15:06:33 -0600

    Resolver algunos errores

--- wp-content/themes/portfolio/page.experience.php
+++ wp-content/themes/portfolio/page.experience.php
@@ -15,7 +15,7 @@
 </div>
 
 <section class="experience-detail">
-    <div class="panel-group" id="accordionDiseno" role="tablist" aria-multiselectable="true">
+    <div class="panel-group" id="accordion" role="tablist" aria-multiselectable="false">
         <?php 
             $args = array(
                 'post_type' => 'experiencia',
@@ -28,13 +28,16 @@
             while($trabajo->have_posts()): $trabajo->the_post();
         ?>
         <div class="panel panel-default">
-            <div class="panel-heading" role="tab" id="diseno">
+	        
+            <div class="panel-heading" role="tab" id="heading<?php echo get_the_ID(); ?>">
                 <h3 class="panel-title">
-                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#<?php echo get_the_ID(); ?>" aria-expanded="false" aria-controls="<?php echo get_the_ID(); ?>" class="collapsed">
+                    <a role="button" data-toggle="collapse" data-parent="#accordion" href="#<?php echo get_the_ID(); ?>" aria-expanded="true" aria-controls="<?php echo get_the_ID(); ?>" class="">
                             <?php the_title() ?> <i class="fa fa-arrow-circle-down" aria-hidden="true"></i></a>
                 </h3>
             </div>
-            <div class="panel-collapse collapse" id="<?php echo get_the_ID(); ?>" role="tabpanel" aria-labelledby="<?php the_title() ?>">
+	        
+	        
+            <div class="panel-collapse collapse" id="<?php echo get_the_ID(); ?>" role="tabpanel" aria-labelledby="heading<?php the_title() ?>">
                 <div class="panel-body">
                     <div class="list-group">
                         <h4 class="list-group-item-heading">Cargo</h4>
@@ -59,7 +62,9 @@
         
 
     <div class="row text-center">
-        <a href="" class="btn btn-default">Descargar CV</a>
+        <a href="https://www.dropbox.com/s/64s1i0r9zmxglco/Treisy%20Jim%C3%A9nez%20Vega%20-%20En.pdf?dl=0" class="btn btn-default" target="_blank">
+	        <i class="fa fa-cloud-download" aria-hidden="true"></i>  Descargar CV
+        </a>
     </div>
 </section>
 <?php get_footer(); ?>
--- wp-content/themes/portfolio/page.php
+++ wp-content/themes/portfolio/page.php
@@ -1,5 +1,43 @@
 <?php get_header(); ?>
 
-    <h1>page.php</h1>
+<?php
+$pagina_blog = get_option( 'page_for_posts');
+$imagen = get_post_thumbnail_id( $pagina_blog );
+$imagen = wp_get_attachment_image_src( $imagen, full );
+?>
+
+<div class="hero-image light-theme" style="background-image:url(<?php echo $imagen[0] ?>)">
+	<div class="text-wrapper">
+		<h1 class="text-center title-section"><?php echo get_the_title( $pagina_blog ) ?></h1>
+	</div>
+</div>
+
+
+<section class="portfolio-list">
+
+	<div class="row">
+		
+		<?php while(have_posts()): the_post(); ?>
+			<div class="col-xs-12 col-md-6 col-lg-4">
+				<div class="panel panel-default">
+					<div class="panel-body">
+						<a href="<?php the_permalink(); ?>">
+							<div class="image" style="background-image:url('<?php the_post_thumbnail_url('full'); ?>')"></div>
+						</a>
+					</div>
+					<div class="panel-footer text-center">
+						<a href="<?php the_permalink(); ?>">
+							<?php the_title( '<span>', '</span>' ); ?>
+						</a>
+					</div>
+				</div>
+			</div>
+		
+		<?php endwhile; ?>
+
+	</div>
+</section>
+
+
 
 <?php get_footer(); ?>
